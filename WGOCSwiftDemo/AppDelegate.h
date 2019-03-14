f942f1bbdf9c29b77cb85f24c7a83bbb33e93953
王刚 <wanggang@317hu.com>
2019-03-14 15:37:46 +0800

    Initial Commit

---
+++ WGOCSwiftDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGOCSwiftDemo
+//
+//  Created by wanggang on 2019/3/14.
+//  Copyright © 2019 bozhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
